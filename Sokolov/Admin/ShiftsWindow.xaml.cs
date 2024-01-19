57b46bda4549ecace61b685b33d4ee732b2dbcae
Скосырев Михаил Андреевич <Skosyrev.M.A@corp.kp11.ru>
2024-01-19 15:54:59 +0300

    Created add new shift window

---
+++ Sokolov/Admin/ShiftsWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sokolov.Admin
+{
+    /// <summary>
+    /// Interaction logic for ShiftsWindow.xaml
+    /// </summary>
+    public partial class ShiftsWindow : Window
+    {
+        public ShiftsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNewShift(object sender, RoutedEventArgs e)
+        {
+            AddShiftWindow addShiftWindow = new AddShiftWindow();
+            addShiftWindow.Show();
+            this.Close();
+        }
+    }
+}
