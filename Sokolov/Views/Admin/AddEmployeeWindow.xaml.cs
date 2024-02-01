bc2139b1156f342bdb909571b96bce2e319e3535
Скосырев Михаил Андреевич <Skosyrev.M.A@corp.kp11.ru>
2024-02-01 15:52:52 +0300

    Created add employee function

--- Sokolov/Views/Admin/AddEmployeeWindow.xaml.cs
+++ Sokolov/Views/Admin/AddEmployeeWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Sokolov.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +22,63 @@ namespace Sokolov.Views.Admin
     /// </summary>
     public partial class AddEmployeeWindow : Window
     {
+        private readonly SokolovContext _context;
+        public event EventHandler EmployeeAdded;
+
+        private ObservableCollection<Role> _roles;
+
+
         public AddEmployeeWindow()
         {
             InitializeComponent();
+            _context = new SokolovContext();
+            LoadRoles();
+        }
+
+        private void LoadRoles()
+        {
+            _roles = new ObservableCollection<Role>(_context.Roles.ToList());
+            RoleCmb.ItemsSource = _roles;
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTxb.Text;
+            string surname = SurnameTxb.Text;
+            string login = LoginTxb.Text;
+            string password = PasswordTxb.Text;
+            string roleName = RoleCmb.DisplayMemberPath.ToString();
+
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(surname) || string.IsNullOrWhiteSpace(login) 
+                || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(roleName))
+            {
+                MessageBox.Show("Заполните все поля.");
+                return;
+            }
+
+            Role role = _context.Roles.FirstOrDefault(r => r.Name == roleName);
+            if (role == null)
+            {
+                MessageBox.Show("Выбранная роль не найдена.");
+                return;
+            }
+
+            User newUser = new User
+            {
+                Name = name,
+                Surname = surname,
+                Login = login.ToLower(),
+                Password = password,
+                RoleId = role.Id,
+                Status = "Active",
+            };
+
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+
+            EmployeeAdded?.Invoke(this, EventArgs.Empty);
+            MessageBox.Show("Новый сотрудник успешно добавлен");
+            this.Close();
         }
     }
 }
