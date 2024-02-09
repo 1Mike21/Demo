2ecc79d77c6e16ee43f89b2bb1f18b857ead4585
Скосырев Михаил Андреевич <Skosyrev.M.A@corp.kp11.ru>
2024-02-09 15:58:39 +0300

    Created functional create new order

--- Sokolov/Views/Waiter/AddOrderWindow.xaml.cs
+++ Sokolov/Views/Waiter/AddOrderWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Sokolov.Models;
+using Sokolov.Views.Admin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,55 @@ namespace Sokolov.Views.Waiter
     /// </summary>
     public partial class AddOrderWindow : Window
     {
+        private readonly SokolovContext _context;
         public AddOrderWindow()
         {
             InitializeComponent();
+            _context = new SokolovContext();
+
+            BackBtn.Click += (sender, e) =>
+            {
+                OrdersWindow ordersWindow = new OrdersWindow();
+                ordersWindow.Show();
+                this.Close();
+            };
+
+            var products = _context.Products.ToList();
+            ProductsListBox.ItemsSource = products;
+        }
+
+        private async void CreateOrderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var selectedProducts = ProductsListBox.SelectedItems.Cast<Product>().ToList();
+
+                var place = PlaceTxb.Text;
+                var countPerson = int.Parse(CountPersonTxb.Text);
+                
+                var newOrder = new Order
+                {
+                    Place = place,
+                    CountPerson = countPerson,
+                    Status = "Принят",
+                    Date = DateTime.Now,
+                };
+
+                foreach (var product in selectedProducts)
+                {
+                    newOrder.OrderProducts.Add(new OrderProduct { Product = product });
+                }
+
+                _context.Orders.Add(newOrder);
+                await _context.SaveChangesAsync();
+
+                MessageBox.Show("Заказ успешно создан", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при создании заказа: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
--- Sokolov/Views/Waiter/WaiterWindow.xaml.cs
+++ Sokolov/Views/Waiter/WaiterWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Sokolov.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,44 @@ namespace Sokolov.Views.Waiter
     /// </summary>
     public partial class WaiterWindow : Window
     {
+        private readonly SokolovContext _context;
+
         public WaiterWindow()
         {
             InitializeComponent();
+            _context = new SokolovContext();
+            LoadOrdersAsync();
+
+            CreateOrderBtn.Click += (sender, e) =>
+            {
+                AddOrderWindow addOrderWindow = new AddOrderWindow();
+                addOrderWindow.Show();
+                this.Close();
+            };
+        }
+
+        public List<Order> Orders { get; set; }
+
+        private async void LoadOrdersAsync()
+        {
+            Orders = await _context.Orders.Include(o => o.User).Include(o => o.OrderProducts).ThenInclude(o => o.Product).ToListAsync();
+            CurrentOrdersGrid.ItemsSource = Orders;
+        }
+
+        private void ChangeOrderStatusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedOrder = CurrentOrdersGrid.SelectedItem as Order;
+            if (selectedOrder != null && selectedOrder.Status == "Готов")
+            {
+                selectedOrder.Status = "Оплачен";
+                _context.SaveChanges();
+                MessageBox.Show("Статус заказа успешно сменен", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                LoadOrdersAsync();
+            }
+            else
+            {
+                MessageBox.Show("Выберите заказ для смены статуса!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
     }
 }
